3190e6456cc2279e795d4505c9c5315047f183bc
Joan Marc Carbo Arnau <jmcarbo@gmail.com>
2011-01-21 15:38:50 +0100

    Added webrat dependency to rspec

--- lib/template/recipes/rspec.rb
+++ lib/template/recipes/rspec.rb
@@ -1,10 +1,11 @@
 gem 'rspec-rails', '>= 2.4.1', :group => [:development]
+gem 'webrat', :group => [:test]
 
 post_bundler_strategies << lambda do
   generate 'rspec:install'
-  
+
   spec_helper_path = 'spec/spec_helper.rb'
-    
+
   gsub_file spec_helper_path, 'config.fixture_path = "#{::Rails.root}/spec/fixtures"', ''
   gsub_file spec_helper_path, /(config.use_transactional_fixtures = true)/, '# \1'
   inject_into_file spec_helper_path, load_snippet('mongoid', 'rspec'), :after => "# config.use_transactional_fixtures = true\n"
