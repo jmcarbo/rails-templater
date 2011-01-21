11c176f88c60db9e205c64644724d423f992796d
Joan Marc Carbo <jmcarbo@gmail.com>
2011-01-22 00:03:34 +0100

    Error in adding devise fixed

--- lib/template/recipes/devise.rb
+++ lib/template/recipes/devise.rb
@@ -1,5 +1,7 @@
-gem 'devise'
+if template_options[:devise]==true
+  gem 'devise'
 
-post_bundler_strategies << lambda do
-  generate 'devise:install'
+  post_bundler_strategies << lambda do
+    generate 'devise:install'
+  end
 end
